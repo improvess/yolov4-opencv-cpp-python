d452ff8ed7f989b4fef54730a01ecbe01fa44395
doleron <doleron@gmail.com>
2022-01-16 00:43:23 -0300

    initial import

---
+++ cpp/yolo.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <queue>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+
+#include <opencv2/core.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+/*
+ * compile it with: g++ -O3 src/yolo.cpp -o yolo_example `pkg-config --cflags --libs opencv4`
+ *
+ * and run it using:
+ * 
+ * .yolo_example
+ */
+
+std::vector<std::string> load_class_list() {
+    std::vector<std::string> class_list;
+    std::ifstream ifs("config_files/classes.txt");
+    std::string line;
+    while (getline(ifs, line))
+    {
+        class_list.push_back(line);
+    } 
+    return class_list;
+}
+
+const std::string YOLO_VERSION = "v4-tiny";
+
+void load_net(cv::dnn::Net &net) {
+    auto result = cv::dnn::readNetFromDarknet("config_files/yolo" + YOLO_VERSION + ".cfg", "config_files/yolo" + YOLO_VERSION + ".weights");
+    net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+    net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+
+    net = result;
+}
+
+const std::vector<cv::Scalar> colors = {cv::Scalar(255, 255, 0), cv::Scalar(0, 255, 0), cv::Scalar(0, 255, 255), cv::Scalar(255, 0, 0)};
+
+int main(int argc, char ** argv)
+{
+
+    std::vector<std::string> class_list = load_class_list();
+
+    cv::Mat frame;
+    cv::VideoCapture capture("sample.mp4");
+    if(!capture.isOpened()){
+	    std::cout << "Error opening video file\n";
+	    return -1;
+	}
+
+    cv::dnn::Net net;
+    load_net(net);
+
+    auto model = cv::dnn::DetectionModel(net);
+    model.setInputParams(1./255, cv::Size(416, 416), cv::Scalar(), true);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    int frame_count = 0;
+    float fps = -1;
+
+    while(true)
+    {
+        capture.read(frame);
+        if (frame.empty())
+        {
+            std::cout << "End of stream\n";
+            break;
+        }
+
+        std::vector<int> classIds;
+        std::vector<float> confidences;
+        std::vector<cv::Rect> boxes;
+        model.detect(frame, classIds, confidences, boxes, 0.2, 0.4);
+        frame_count++;
+
+        int detections = classIds.size();
+
+        for (int i = 0; i < detections; ++i) {
+
+            auto box = boxes[i];
+            auto classId = classIds[i];
+            const auto color = colors[classId % colors.size()];
+            cv::rectangle(frame, box, color, 3);
+
+            cv::rectangle(frame, cv::Point(box.x, box.y - 20), cv::Point(box.x + box.width, box.y), color, cv::FILLED);
+            cv::putText(frame, class_list[classId].c_str(), cv::Point(box.x, box.y - 5), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+        }
+
+        if (frame_count >= 30) {
+
+            auto end = std::chrono::high_resolution_clock::now();
+            fps = frame_count * 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+            frame_count = 0;
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        if (fps > 0) {
+
+            std::ostringstream fps_label;
+            fps_label << std::fixed << std::setprecision(2);
+            fps_label << "FPS: " << fps;
+            std::string fps_label_str = fps_label.str();
+
+            cv::putText(frame, fps_label_str.c_str(), cv::Point(10, 25), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
+            
+        }
+
+        cv::imshow("output", frame);
+
+        if(cv::waitKey(1) != -1) {
+            capture.release();
+            std::cout << "finished by user\n";
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ python/yolo.py
@@ -0,0 +1,63 @@
+import cv2
+import time
+
+def build_model():
+    net = cv2.dnn.readNet("config_files/yolov4-tiny.weights", "config_files/yolov4-tiny.cfg")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+    model = cv2.dnn_DetectionModel(net)
+    model.setInputParams(size=(416, 416), scale=1./255, swapRB=True)
+    return model
+
+def load_capture():
+    capture = cv2.VideoCapture("sample.mp4")
+    return capture
+
+def load_classes():
+    class_list = []
+    with open("config_files/classes.txt", "r") as f:
+        class_list = [cname.strip() for cname in f.readlines()]
+    return class_list
+
+colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
+
+model = build_model()
+capture = load_capture()
+class_list = load_classes()
+
+start = time.time_ns()
+frame_count = 0
+fps = -1
+
+while True:
+
+    _, frame = capture.read()
+    if frame is None:
+        print("End of stream")
+        break
+
+    classIds, confidences, boxes = model.detect(frame, .2, .4)
+    frame_count += 1
+
+    for (classid, confidence, box) in zip(classIds, confidences, boxes):
+        color = colors[int(classid) % len(colors)]
+        cv2.rectangle(frame, box, color, 2)
+        cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+        cv2.putText(frame, class_list[classid[0]], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+
+    if frame_count >= 30:
+        end = time.time_ns()
+        fps = 1000000000 * frame_count / (end - start)
+        frame_count = 0
+        start = time.time_ns()
+    
+    if fps > 0:
+        fps_label = "FPS: %.2f" % fps
+        cv2.putText(frame, fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow("output", frame)
+
+    if cv2.waitKey(1) > -1:
+        print("finished by user")
+        break
